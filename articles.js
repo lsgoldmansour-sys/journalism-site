e0fbed8f186245d2c934538c7d70a48d79c9be8b
lsgoldmansour-sys <lsgoldmansour@gmail.com>
2026-05-06 15:07:00 -0400

    Add files via upload

---
+++ articles.js
@@ -0,0 +1,33 @@
+const articles = [
+  { t:"CUNY Is Investigating Student Government Leaders for Boycotting Israel", c:"Palestine", o:"The Nation", l:"https://www.thenation.com/article/activism/cuny-title-vi-investigation-palestine-student-government-boycott-israel/" },
+
+  { t:"CUNY Faculty Are Still Determined to Hire Palestinian Studies Scholars", c:"Education", o:"The Nation", l:"https://www.thenation.com/article/activism/cuny-hunter-college-palestinian-studies-job-hochul/" },
+
+  { t:"Mahmoud Khalil Is “Ready to Fight” His Unlawful Detention", c:"Palestine", o:"The Nation", l:"https://www.thenation.com/article/politics/mahmoud-khalil-shezza-abboushi-dallal-interview/" },
+
+  { t:"CUNY’s Repression of Pro-Palestine Students Continues in Front of Congress", c:"Palestine", o:"The Nation", l:"https://www.thenation.com/article/politics/cuny-house-republicans-hearing-repression/" },
+
+  { t:"Democrats Rack up Key Wins to Buck Trump", c:"Electoral Politics", o:"In These Times", l:"https://inthesetimes.com/article/10-races-to-watch-this-election-day-november-2025" },
+
+  { t:"Mayor Mamdani and New Yorkers Want to Tax the Rich. Where Is City Council Speaker Julie Menin?", c:"New York", o:"In These Times", l:"https://inthesetimes.com/article/zohran-mamdani-julie-menin-kathy-hochul-nyc-tax-rich" },
+
+  { t:"Kingston Agreed the Rent Was Too Damn High—So It Lowered It", c:"Housing", o:"Shelterforce", l:"https://shelterforce.org/2024/10/11/kingston-agreed-the-rent-was-too-damn-high-so-it-lowered-it/" },
+
+  { t:"Florida Organizers Look for Ways to Help Tenants Despite a Hostile State Government", c:"Housing", o:"Shelterforce", l:"https://shelterforce.org/2024/11/21/florida-organizers-look-for-ways-to-help-tenants-despite-a-hostile-state-government/" },
+
+  { t:"AOC Urges Activism on Earth Day", c:"Climate", o:"Harlem View", l:"https://harlemview.com/community/2021/05/aoc-urges-activism-on-earth-day/" },
+
+  { t:"Amazon Labor Union Leads Organizing Wave", c:"Labor", o:"Harlem View", l:"https://harlemview.com/community/jobs/2022/05/amazon-labor-union-leads-organizing-wave/" },
+
+  { t:"CUNY City College President Laments Not Breaking Up Pro-Palestinian Encampment Sooner", c:"Palestine", o:"The City", l:"https://www.thecity.nyc/2024/05/13/city-college-palestine-encampment-nypd/" },
+
+  { t:"Who Wants to Teach in Florida?", c:"Education", o:"The American Prospect", l:"https://prospect.org/2023/02/23/02-22-2023-desantis-education-teacher-vacancies/" },
+
+  { t:"New York Considers Community Land Trusts", c:"Housing", o:"The American Prospect", l:"https://prospect.org/2023/05/03/2023-05-03-new-york-community-land-trusts/" },
+
+  { t:"How New York’s Democratic Socialists Brought Unions Around to Public Renewables", c:"Climate", o:"The American Prospect", l:"https://prospect.org/2023/06/19/2023-06-19-new-york-democratic-socialists-unions-public-renewables/" },
+
+  { t:"CUNY Gave $4 Million Contract to Security Firm That Claims Protesters Are Using ‘Guerrilla Warfare Tactics’", c:"Palestine", o:"Hellgate NYC", l:"https://hellgatenyc.com/cuny-contract-security-firm-protesters-guerrilla-warfare/" },
+
+  { t:"Blaming Workers, Hiding Profits in Primetime Inflation Coverage", c:"Media Criticism", o:"FAIR", l:"https://fair.org/home/blaming-workers-hiding-profits-in-primetime-inflation-coverage/" }
+];
